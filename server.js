71add1c9469fae161c57d18d3e4e29274d3fcd26
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-03-02 14:39:47 -0300

    Cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/apinode", { useNewUrlParser: true });
 
