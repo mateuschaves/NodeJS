a3390edb556857dfd3edfaae7b7763b2d445fea6
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-03-02 14:11:18 -0300

    Rota para cadastro de produtos

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 mongoose.connect("mongodb://localhost:27017/apinode", { useNewUrlParser: true });
 
