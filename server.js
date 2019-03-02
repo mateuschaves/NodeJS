a56e00b6274284db7036db1a4be4c6e23813066b
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-03-02 13:42:25 -0300

    Organizando o projeto e criando o primeiro controller

--- server.js
+++ server.js
@@ -8,18 +8,7 @@ mongoose.connect("mongodb://localhost:27017/apinode", { useNewUrlParser: true })
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-    
-    Product.create({
-        title: 'teste',
-        description: 'teste',
-        url: 'teste'
-    });
-    
-    return res.send('Opaa, turu pomm ? ');
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001, () => {
     console.log(`Server started on port ${3001}`);
